3bf13ba2eb3eae561fc8463bf5088f5876693d5b
Fabricio Borgobello <fborgobello@gmail.com>
2023-10-29 12:10:46 +0100

    Add fonts and global CSS

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+// Font subsets: https://fonts.google.com/knowledge/glossary/subsetting
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
